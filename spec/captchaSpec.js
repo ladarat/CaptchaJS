b6f676e79396b853e706addf0bb42b1ddea80b96
ladarat <ladarat.y@gmail.com>
2016-04-17 15:00:19 +0700

    add spec operator

--- spec/captchaSpec.js
+++ spec/captchaSpec.js
@@ -31,4 +31,37 @@ describe("Captcha", function() {
         //Assert
         expect(result).toEqual("3 + ONE");
     });
+
+    it("should show 1 - ONE correctly", function() {
+        //Arrange
+        var captcha = new Captcha(1,1,2,1);
+
+        //Act
+        var result = captcha.show();
+
+        //Assert
+        expect(result).toEqual("1 - ONE");
+    });
+
+    it("should show 1 * ONE correctly", function() {
+        //Arrange
+        var captcha = new Captcha(1,1,3,1);
+
+        //Act
+        var result = captcha.show();
+
+        //Assert
+        expect(result).toEqual("1 * ONE");
+    });
+
+    it("should show 1 / ONE correctly", function() {
+        //Arrange
+        var captcha = new Captcha(1,1,4,1);
+
+        //Act
+        var result = captcha.show();
+
+        //Assert
+        expect(result).toEqual("1 / ONE");
+    });
 });
--- src/Captcha.js
+++ src/Captcha.js
@@ -1,5 +1,16 @@
 function Captcha(pattern, leftOpr, operator, rightOpr){   
     this.show = function (){
-        return leftOpr + " + ONE";
+        if(operator === 1){
+            return leftOpr + " + ONE";
+        }
+        if(operator === 2){
+            return leftOpr + " - ONE";
+        }
+        if(operator === 3){
+            return leftOpr + " * ONE";
+        }
+        if(operator === 4){
+            return leftOpr + " / ONE";
+        }
     };
 };
